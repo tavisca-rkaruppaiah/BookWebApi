c0bd711ad1e5f2f7b9f0e682b836ef97f10ff64d
Karuppaiah <rkaruppaiah@internal.tavisca.com>
2019-09-05 17:36:12 +0530

    added by ragu

---
+++ BookWebApi/Models/BookDetails.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookWebApi.Models
+{
+    class BookDetails
+    {
+        private static List<Book> books = new List<Book>();
+        public void Add(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void Remove(int id)
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].id == id)
+                    books.RemoveAt(i);
+            }
+        }
+
+        public Book GetById(int id)
+        {
+            for(int i = 0; i<books.Count; i++)
+            {
+                if (books[i].id == id)
+                    return books[i];
+            }
+            return null;
+        }
+
+        public List<Book> Get()
+        {
+            return books;
+        }
+
+        public void UpdateInformation(int id, Book book)
+        {
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].id == id)
+                    books[i] = book;
+            }
+        }
+    }
+}
---
+++ BookWebApi/Models/Validation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BookWebApi.Models
+{
+    public class Validation
+    {
+        public static bool IsPositiveNumber(int id)
+        {
+            return id > 0;
+        }
+
+        public static bool IsContainsLettersOnly(string value)
+        {
+            return Regex.IsMatch(value, @"^[a-zA-Z]+$");
+        }
+    }
+}
